3a21c363aca955c4b32e83820af2f3dae2549ae8
Joanna Rahhal <joanna.h.rahhal@gmail.com>
2024-07-29 20:35:18 +0300

    Added Anchor link to navigation

--- client/src/components/MobileMenu.jsx
+++ client/src/components/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import { CiMenuFries } from "react-icons/ci";
 import { RiCloseLargeFill } from "react-icons/ri";
 
@@ -20,10 +20,10 @@ const MobileMenu = ({ onClick, toggle, handleClose }) => {
             className="xs-mobile:font-extrabold xs-mobile:text-2xl xs-mobile:self-start ml-2 sm-tablet:text-3xl"
             onClick={handleClose}
           />
-          <Link to="#">Home</Link>
-          <Link to="#">Menu</Link>
-          <Link to="#">Our Story</Link>
-          <Link to="#">Events</Link>
+          <AnchorLink href="#home">Home</AnchorLink>
+          <AnchorLink href="#menu">Menu</AnchorLink>
+          <AnchorLink href="#our-story">Our Story</AnchorLink>
+          <AnchorLink href="#events">Events</AnchorLink>
         </div>
       )}
     </>
